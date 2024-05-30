1539ecd60c806990a763a977fa22f7774ee59ecd
azesm <azesm@yahoo.com>
2024-05-30 21:46:09 +0300

    oop/less1

--- src/com/vladan/oop/less1/TestEx.java
+++ src/com/vladan/oop/less1/TestEx.java
@@ -1,2 +1,16 @@
-package com.vladan.oop.less1;public class TestEx {
+package com.vladan.oop.less1;
+
+public class TestEx {
+    public static void main(String[] args) {
+        int value = 10;
+        int [] values = {value, 100, 199, 25};
+        printArray(values);
+    }
+
+    private static void printArray(int[] values) {
+        for (int value : values) {
+            System.out.println(value);
+
+        }
+    }
 }
