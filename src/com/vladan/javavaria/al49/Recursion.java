24467e652c497729dfd1a003770f42201a6e4985
azesm <azesm@yahoo.com>
2024-05-31 16:50:28 +0300

    Alishev_1-49_Recursion

---
+++ src/com/vladan/javavaria/al49/Recursion.java
@@ -0,0 +1,18 @@
+package com.vladan.javavaria.al49;
+
+public class Recursion {
+    public static void main(String[] args) {
+
+        // 4! = 4 * 3 * 2 * 1
+
+        System.out.println(factorial(4));
+    }
+
+    private static int factorial(int n) {
+        if (n == 1)
+            return 1;
+
+        return n * factorial(n - 1);
+    }
+
+}
